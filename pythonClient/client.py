771c504a2c002ca5963fd0f224f5543170f4a2fd
Ricky-Bonder <rickyossola@yahoo.it>
2023-05-04 17:25:46 +0200

    client writes pub key in characteristic

--- pythonClient/client.py
+++ pythonClient/client.py
@@ -1,14 +1,77 @@
-import pygatt
+import bleak
+import asyncio
+from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey, X25519PublicKey
+from cryptography.hazmat.primitives import serialization
 import time
-adapter = pygatt.BLEDevice('00:1A:7D:DA:71:15')
-try:
-    adapter.start()
-    adapter.scan(timeout=5)
-    device = adapter.connect('00:1A:7D:DA:71:15',address_type=pygatt.BLEAddressType.public)
-    characteristic = "12346677-0000-1000-8000-00805F9B34FB"
-    device.char_write_handle(characteristic, bytearray([0x00]), wait_for_response=True)
-    time.sleep(2)
-    value = device.char_read_handle(characteristic)
-    print(value)
-finally:
-    adapter.stop()
+import base64
+
+def device_sort(device):
+    return device.address
+
+async def connect():
+    # try:
+    #      devices = await bleak.BleakScanner.discover()
+    # except bleak.exc.BleakDBusError as e:
+    #     if str(e) == '[org.bluez.Error.NotReady] Resource Not Ready':
+    #         raise RuntimeError('Bluetooth is not ready. Maybe try `bluetoothctl power on`?')
+    #     raise
+    # devices.sort(key=device_sort)
+    # print('==== BLE Discovery results ====')
+    # print('{0: >4} {1: <33} {2: <12}'.format(
+    #     'S.N.', 'Name', 'Address'))
+    # for i, _ in enumerate(devices):
+    #     print('[{0: >2}] {1: <33} {2: <12}'.format(i + 1, devices[i].name or 'Unknown', devices[i].address))
+
+    public_key = generate_key_pair()
+
+    public_key_bytes = public_key.public_bytes(
+        encoding = serialization.Encoding.Raw,
+        format = serialization.PublicFormat.Raw
+    )
+
+    public_key_base64 = base64.b64encode(public_key_bytes)
+
+    print(public_key_bytes)
+    print('Connecting...')
+    device = bleak.BleakClient('AC:82:47:1C:22:98', timeout=100)
+    KEY_EXCHANGE_CHAR_UUID = "12346677-0000-1000-8000-00805F9B34FB"
+    await device.connect()
+    
+    time.sleep(1)
+    print('Connected, writing public key in char...')
+    await device.write_gatt_char(KEY_EXCHANGE_CHAR_UUID, public_key_base64)
+
+    print('DONE')
+    device.disconnect()
+    # services = await device.services
+    # for i, _ in enumerate(services):
+    #     print(services[i])
+
+def generate_key_pair():
+    # Generate a private key
+    private_key = X25519PrivateKey.generate()
+
+    # Derive the public key from the private key
+    public_key = private_key.public_key()
+
+    # Print the private and public keys in PEM format
+    print("Private key (PEM format):")
+    print(private_key.private_bytes(
+        encoding = serialization.Encoding.PEM,
+        format = serialization.PrivateFormat.PKCS8,
+        encryption_algorithm = serialization.NoEncryption()
+    ))
+
+    print("Public key (PEM format):")
+    print(public_key.public_bytes(
+        encoding = serialization.Encoding.PEM,
+        format = serialization.PublicFormat.SubjectPublicKeyInfo
+    ))
+
+    return public_key
+
+async def main():
+    await connect()
+
+if __name__ == '__main__':
+    asyncio.run(main())
