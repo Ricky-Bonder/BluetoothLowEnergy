2ffc23d3d18b0cc8e066dd38433dd90ca6c43fbc
Ricky-Bonder <rickyossola@yahoo.it>
2023-05-22 17:12:36 +0200

    server decodes and decrypts session token

--- encryption.go
+++ encryption.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/rand"
 	"crypto/sha256"
 	"fmt"
@@ -14,7 +16,7 @@ import (
 var g_dev_pubkey [32]byte
 var g_dev_privkey [32]byte
 var g_randomBytes = make([]byte, 32)
-var g_shared_key = make([]byte, 0)
+var g_session_key = make([]byte, 0)
 
 /*
 GenerateKey generates a public private key pair using Curve25519.
@@ -62,7 +64,7 @@ func GenerateSharedSecretWithPoP(priv, pub, pop []byte) error {
 		secret[i] ^= popHash[i]
 	}
 
-	g_shared_key = secret
+	g_session_key = secret
 
 	return nil
 }
@@ -74,4 +76,35 @@ func GenarateInitializationVector() error {
 		return err
 	}
 	return nil
+}
+
+func decryptToken(token []byte, sessionKey []byte) ([]byte, error) {
+	// Create a new AES cipher block
+	block, err := aes.NewCipher(sessionKey)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create a new AES-GCM mode cipher with the block
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	// Nonce size must be unique for every encryption, but doesn't need to be secret
+	nonceSize := aesgcm.NonceSize()
+	if len(token) < nonceSize {
+		return nil, fmt.Errorf("invalid token")
+	}
+
+	// Extract the nonce from the token
+	nonce := token[:nonceSize]
+
+	// Decrypt the token using AES-GCM
+	decrypted, err := aesgcm.Open(nil, nonce, token[nonceSize:], nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return decrypted, nil
 }
